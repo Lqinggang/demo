ecbd9c76466503b8b4c25d5617998b2246a38522
liaoqinggang <liaoqinggang@p2w.net.cn>
2019-01-18 10:27:00 +0800

    added daemon creation; introduce missing header files;

--- network/connect_retry.c
+++ network/connect_retry.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/socket.h>
 
 #define MAXSLEEP	128
--- network/initserver.c
+++ network/initserver.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <unistd.h>
 #include <errno.h>
 #include <sys/socket.h>
 
