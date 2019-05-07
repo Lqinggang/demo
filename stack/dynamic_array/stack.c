669aae05fb0d6057f853f14857cd6dc494f8ca2c
lqinggang <1944058861@qq.com>
2019-05-07 22:55:00 +0800

    update dynamic array based stack

--- stack/dynamic_array/stack.c
+++ stack/dynamic_array/stack.c
@@ -1,88 +1,82 @@
 #include "stack.h"
 #include <stdlib.h>
 
+static STACK_TYPE  *stack = NULL;
+static int top_element = -1;
+static size_t stack_size = 0;
+
 pstack
 create_stack(size_t size)
 {
-    pstack stack = NULL;
-    if ((stack = (pstack)malloc(sizeof(pstack))) == NULL)
+    if ((stack = (STACK_TYPE *)malloc(size)) != NULL)
     {
-        return (NULL);
+        stack_size = size;
     }
-
-    if ((stack->stack = (STACK_TYPE *)malloc(size)) == NULL)
-    {
-        free(stack);
-        return (NULL); }
-    stack->top_element = -1;
-    stack->stack_size = size;
 
     return (stack);
 }
 
 int
-destroy_stack(pstack stack)
+destroy_stack(void)
 {
     if (stack != NULL)
     {
-        if (stack->stack != NULL)
-        {
-            free(stack->stack);
-        }
+        stack_size = 0;
         free(stack);
+        stack = NULL;
         return (0);
     }
     return (-1);
 }
 
 int
-push(pstack stack, STACK_TYPE value)
+push(STACK_TYPE value)
 {
-    if (stack != NULL && stack->stack != NULL && is_full(stack) == 0)
+    if (stack != NULL && is_full() == 0)
     {
-        stack->stack[++stack->top_element] = value;
+        stack[++top_element] = value;
         return (0);
     }
     return (-1);
 }
 
 STACK_TYPE *
-pop(pstack stack)
+pop(void)
 {
-    if (stack != NULL && stack->stack != NULL && is_empty(stack) == 0)
+    if (stack != NULL && is_empty() == 0)
     {
-        return (&stack->stack[stack->top_element--]);
+        return (&stack[top_element--]);
     }
     return (NULL);
 }
 
 STACK_TYPE *
-top(const pstack stack)
+top(void)
 {
-    if (stack != NULL && stack->stack != NULL && is_empty(stack) == 0)
+    if (stack != NULL && is_empty() == 0)
     {
-        return (&stack->stack[stack->top_element]);
+        return (&stack[top_element]);
     }
     return (NULL);
 }
 
 int
-is_empty(const pstack stack)
+is_empty(void)
 {
     if (stack != NULL)
     {
-        return (stack->top_element == -1);
+        return (top_element == -1);
     }
     return (-1);
 }
 
 int
-is_full(const pstack stack)
+is_full(void)
 {
     if (stack != NULL)
     {
-        //Warning
-        return ((size_t)stack->top_element == stack->stack_size);
+        // Warning
+        return (top_element == stack_size - 1);
     }
     return (-1);
 }
--- stack/dynamic_array/stack.h
+++ stack/dynamic_array/stack.h
@@ -4,19 +4,14 @@
 #include <stdio.h>
 
 typedef int STACK_TYPE;
-
-typedef struct stack {
-    STACK_TYPE *stack; 
-    int top_element;
-    size_t stack_size;
-} *pstack;
+typedef STACK_TYPE * pstack;
 
 extern pstack create_stack(size_t size);
-extern int destroy_stack(pstack stack);
-extern int push(pstack stack, STACK_TYPE value);
-extern STACK_TYPE *pop(pstack stack);
-extern STACK_TYPE *top(const pstack stack);
-extern int is_empty(const pstack stack);
-extern int is_full(const pstack stack);
+extern int destroy_stack(void);
+extern int push(STACK_TYPE value);
+extern STACK_TYPE *pop(void);
+extern STACK_TYPE *top(void);
+extern int is_empty(void);
+extern int is_full(void);
 
 #endif  /* STACK_DYNAMIC_ARRAY_H */
